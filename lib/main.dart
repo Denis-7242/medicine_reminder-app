c817e1c38bc9005d86c46af2f5b14ea7ee208336
Denis-7242 <dexdenis3@gmail.com>
2025-10-29 16:02:51 +0300

    updated the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
             seedColor: Colors.teal,
             brightness: Brightness.light,
           ),
-          cardTheme: CardTheme(
+          cardTheme: CardThemeData(
             elevation: 2,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
@@ -57,7 +57,7 @@ class MyApp extends StatelessWidget {
             seedColor: Colors.teal,
             brightness: Brightness.dark,
           ),
-          cardTheme: CardTheme(
+          cardTheme: CardThemeData(
             elevation: 2,
             color: const Color(0xFF1E1E1E),
             shape: RoundedRectangleBorder(
